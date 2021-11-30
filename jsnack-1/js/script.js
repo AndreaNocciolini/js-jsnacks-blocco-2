824fe27408916817c73121a0a07bc30ee50ce6d6
AndreaNocciolini <andreanocciolini93@gmail.com>
2021-11-30 10:03:11 +0100

    start

---
+++ jsnack-1/js/script.js
@@ -0,0 +1,6 @@
+const containerFluid = document.querySelector(`.container-fluid`);
+const container = document.querySelector(`.container`);
+
+// jsnack 1
+// Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una lista di cognomi, Gatsby vuole generare una falsa lista di invitati.
+
