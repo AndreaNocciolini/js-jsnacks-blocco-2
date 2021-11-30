d9f20ff0a74f0fd2ab8b8daa672e3ee783376428
AndreaNocciolini <andreanocciolini93@gmail.com>
2021-11-30 13:22:16 +0100

    finish

--- jsnack-1/js/script.js
+++ jsnack-1/js/script.js
@@ -4,8 +4,8 @@ const container = document.querySelector(`.container`);
 // jsnack 1
 // Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una lista di cognomi, Gatsby vuole generare una falsa lista di invitati.
 
-const names = [`Andrea`, `Giacomo`, `Lorenzo`, `Rondo`, `Renato`, `Terenzio`, `Filadelfio`, `Federico`, `Daniele`, `Fulvio`, `Luca`, `Sara`, `Sarah`, `Lucia`, `Elisabetta`, `Tommaso`, `Thomas`, `Jacopo`, `Emma`, `Emilio`, `Amedeo`, `Dante`];
-const surnames = [`Rossi`, `Bianchi`, `Gialli`, `Bruschi`, `Delacroix`, `Calcare`, `Granada`, `Savoia`, `Talco`, `Panna`, `Rigoletti`, `Frosini`, `Nannini`, `Muletti`, `Pantalone`, `Marchigiani`, `Fo`, `Guantanamo`, `Primo`, `Ultimo`, `Mustafà`];
+const names = [`Andrea`, `Giacomo`, `Lorenzo`, `Rondo`, `Renato`, `Terenzio`, `Filadelfio`, `Federico`, `Daniele`, `Fulvio`, `Luca`, `Sara`, `Sarah`, `Lucia`, `Elisabetta`, `Tommaso`, `Thomas`, `Jacopo`, `Emma`, `Emilio`, `Amedeo`, `Dante`, `Davide`, `Jacob`, `Matteo`, `Fiorenzo`, `Romeo`, `Giulietta`, `Juliet`, `Laura`, `Francesca`, `Sonia`, `Mauro`, `Cesare`, `Alphons`, `Elizabeth`, `Lorenza`, `Simone`, `Niccolò`, `Frederick`, `Nicolas`, `Liliana`, `Greta`, `Giorgia`, `Rita`];
+const surnames = [`Rossi`, `Bianchi`, `Gialli`, `Bruschi`, `Delacroix`, `Calcare`, `Granada`, `Savoia`, `Talco`, `Panna`, `Rigoletti`, `Frosini`, `Nannini`, `Muletti`, `Pantalone`, `Marchigiani`, `Fo`, `Guantanamo`, `Primo`, `Ultimo`, `Mustafà`, `Mazzola`, `Marchi`, `Lombardi`, `Gretelli`, `Ciucchi`, `Uber`, `Zero`, `Giando`, `Intini`, `Sonichetti`, `Zingari`, `Smith`, `Birilli`, `Giovini`, `Palladio`, `Tugurio`, `Giovanna`, `Rapidi`, `Fox`, `Polpi`, `de Santis`, `Bernadei`, `Scoppola`];
 // let userName = names[Math.floor(Math.random() * names.length)];
 // let userSurname = surnames[Math.floor(Math.random() * surnames.length)];
 
--- jsnack-2/js/script.js
+++ jsnack-2/js/script.js
@@ -4,6 +4,36 @@ const container = document.querySelector(`.container`);
 // jsnack 2
 // Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
 
+//**********************************PRIMO METODO*********************************//
+// const numbers = [];
+// let userNumber;
+// do {
+//    userNumber = parseInt(prompt("Please, choose a number between 1 and 75."));
+// } while (isNaN(userNumber) || userNumber < 1 || userNumber > 75); 
+
+// i = 0;
+// while (i < userNumber) {
+//     let number = Math.floor(Math.random() * 101);
+//     numbers.push(number);
+//     i++;
+// }
+
+// const oddNumbers = numbers.filter(checkOdd);
+// function checkOdd(odd) {
+//     return odd % 2 !== 0;
+// }
+// console.log(`Original Array: ${numbers}`);
+// console.log(`Odd Array: ${oddNumbers}`);
+
+// let sum = 0;
+// for (let x = 0; x < oddNumbers.length; x++) {
+//     sum += oddNumbers[x];
+// }
+// console.log(`Odd Array Element's sum: ${sum}`);
+// container.innerHTML = `Your Lucky Number is: <span class="green">${sum}</span>. Nice!`
+//**********************************PRIMO METODO*********************************//
+
+//*********************************SECONDO METODO********************************//
 const numbers = [];
 let userNumber;
 do {
@@ -11,23 +41,27 @@ do {
 } while (isNaN(userNumber) || userNumber < 1 || userNumber > 75); 
 
 i = 0;
-while (i <= userNumber) {
+while (i < userNumber) {
     let number = Math.floor(Math.random() * 101);
     numbers.push(number);
     i++;
 }
 
-const oddNumbers = numbers.filter(checkOdd);
-function checkOdd(odd) {
-    return odd % 2 !== 0;
-}
-console.log(`Original Array: ${numbers}`);
-console.log(`Odd Array: ${oddNumbers}`);
+function oddArrayIndex(arrayName, emptyIndex) {
+    for (let y = 0; y < arrayName.length; y += 2) {
+        emptyIndex.push(arrayName[y]);
+        }
+    }
+
+let oddPositionNumbers = [];
+oddArrayIndex(numbers, oddPositionNumbers);
+console.log(numbers)
+console.log(oddPositionNumbers)
 
 let sum = 0;
-for (let x = 0; x < oddNumbers.length; x++) {
-    sum += oddNumbers[x];
+for (let x = 0; x < oddPositionNumbers.length; x++) {
+    sum += oddPositionNumbers[x];
 }
 console.log(`Odd Array Element's sum: ${sum}`);
 container.innerHTML = `Your Lucky Number is: <span class="green">${sum}</span>. Nice!`
-
+//*********************************SECONDO METODO********************************//
